a0e0f7975733a47521e5df3103f4141fafa40c9b
ali-ahmed-05 <aliahmed.pluton@gmail.com>
2021-12-29 11:55:55 +0500

    base

---
+++ backend/hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomiclabs/hardhat-waffle");
+require('hardhat-abi-exporter');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ const ALCHEMY_API_KEY = `GKcZh-E7o6PB3gEz0M9fUHPwG4_xHbbj`
+ const privateKey = `669a00a5dcee6b12e70ec23b4a793b14bcb38a0f657ce29ada80b578e14743a7`
+
+module.exports = {
+  solidity: "0.8.0",
+ networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${privateKey}`],
+    },
+  },
+  abiExporter: {
+    sources: './contracts',
+    cache: './cache',
+    artifacts: './artifacts',
+    path: './client/src/contract',
+
+  },
+};
---
+++ backend/scripts/deploy.js
@@ -0,0 +1,126 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const { json } = require("hardhat/internal/core/params/argumentTypes");
+
+  // This is a script for deploying your contracts. You can adapt it to deploy
+// yours, or create new ones.
+async function main() {
+  // This is just a convenience check
+  // if (network.name === "hardhat") {
+  //   console.warn(
+  //     "You are trying to deploy a contract to the Hardhat Network, which" +
+  //       "gets automatically created and destroyed every time. Use the Hardhat" +
+  //       " option '--network localhost'"
+  //   );
+  // }
+
+  // ethers is avaialble in the global scope
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  
+
+  //  let manager_addr = await lasm.manager_addr()
+  //  console.log(manager_addr)
+
+  // NFTCrowdsale = await ethers.getContractFactory("NFTCrowdsale");
+  // nftPreSale = await NFTCrowdsale.deploy();
+  // await nftPreSale.deployed();
+
+  // nftPubSale = await NFTCrowdsale.deploy();
+  // await nftPubSale.deployed();
+
+ 
+
+   NFT = await ethers.getContractFactory("NFT");
+   nft = await NFT.deploy(nftPreSale.address,nftPubSale.address);
+   await nft.deployed();
+
+  // Manager = await ethers.getContractFactory("Manager");
+  // manager = await Manager.attach(manager_addr)
+
+ 
+  console.log("nftPreSale deployed to:", nftPreSale.address);
+  console.log("nftPubSale deployed to:", nftPubSale.address);
+  console.log("nft deployed to:", nft.address);
+
+  // We also save the contract's artifacts and address in the frontend directory
+  saveFrontendFiles(nft);
+}
+//,nftPreSale,nftPubSale,nft
+
+function saveFrontendFiles(lasm,nftPreSale,nftPubSale,nft) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../frontend/src/contracts";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+let config = `
+ const nft_addr = "${nft.address}"
+`
+let data =  JSON.stringify(config)
+fs.writeFileSync(
+  contractsDir + '/addresses.js', JSON.parse(data)
+  
+);
+  // fs.writeFileSync(
+  //   contractsDir + "/contract-address.json",
+  //   JSON.stringify({ LASM: lasm.address ,  nftPreSale: nftPreSale.address ,nftPubSale: nftPubSale.address ,nft: nft.address }
+  //     , undefined, 2)
+    
+  // );
+  // fs.writeFileSync(
+  //   contractsDir + "/contract-address.json",
+  //    JSON.stringify({ nftPreSale: nftPreSale.address }, undefined, 2)
+  // );
+  // fs.writeFileSync(
+  //   contractsDir + "/contract-address.json",
+  //   JSON.stringify({ nftPubSale: nftPubSale.address }, undefined, 2)
+  // );
+  // fs.writeFileSync(
+  //   contractsDir + "/contract-address.json",
+  //   JSON.stringify({ nft: nft.address }, undefined, 2)
+  // );
+
+ 
+  const nftArtifact = artifacts.readArtifactSync("NFT");
+
+
+
+  let abi 
+  
+
+ 
+
+  abi  = `const NFT  = ${JSON.stringify(nftArtifact)}`
+
+  data = JSON.stringify(abi,null,2)
+
+  fs.writeFileSync(
+    contractsDir + '/NFT.js', JSON.parse(data)
+    
+  );
+ 
+
+ 
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+// npx hardhat run scripts\deploy.js --network rinkeby
