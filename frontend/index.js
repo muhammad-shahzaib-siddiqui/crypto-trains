6ef885a32326aad68598267433195001d44dade1
MutahirPluto <mutahir.pluton@gmail.com>
2021-12-29 17:49:03 +0500

    Updated

--- frontend/index.js
+++ frontend/index.js
@@ -21,6 +21,41 @@ const Train = [
   ["Legendary", 160],
 ];
 
+const Common = [
+  ["55", 1375],
+  ["20", 500],
+  ["15", 375],
+  ["9", 225],
+  ["1", 25]
+]
+
+const Rare = [
+  ["55", 825],
+  ["20", 300],
+  ["15", 225],
+  ["9", 135],
+  ["1", 15],
+
+]
+
+const Epic = [
+  ["55", 275],
+  ["20", 100],
+  ["15", 75],
+  ["9", 45],
+  ["1", 5],
+
+]
+
+const Legend = [
+  ["55", 88],
+  ["20", 32],
+  ["15", 24],
+  ["9", 14],
+  ["1", 2],
+
+]
+
 const Stations = [
   ["Common", 300],
   ["Mitic", 200],
@@ -41,7 +76,7 @@ function randomWeight(arr) {
   let total = 0;
   for (let index = 0; index < arr.length; ++index) {
     total += arr[index][1];
-    console.log("total", total);
+    // console.log("total", total);
   }
   // Total in hand, we can now pick a random value akin to our
   // random index from before.
@@ -60,7 +95,7 @@ function randomWeight(arr) {
     total += arr[index][1];
     // If this value falls within the threshold, we're done!
     if (total >= threshold) {
-      console.log("index", arr[index][0]);
+      // console.log("index", arr[index][0]);
       return arr[index][0];
     }
   }
@@ -71,25 +106,334 @@ function randomWeight(arr) {
 //* ********** Create the JSON metadata file ********* //
 //* ************************************************** //
 
-function randomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-for (let index = 0; index < 20; index++) {
+
+for (let index = 0; index < 1; index++) {
   let nftData;
 
   const type = randomWeight(nftType);
 
   if (type === "TRAINS") {
     const trainType = randomWeight(Train);
-    const Hardness = Math.floor(Math.random() * 99);
-    nftData = {
-      nftType: type,
-      trainType: trainType,
-      Hardness: randomInteger(20, 99),
-      Power: randomInteger(20, 99),
-      Speed: randomInteger(20, 99),
-    };
+    if(trainType === "Common"){
+     let Hardness;
+     let Power;
+     let PowerSpeed;
+
+      const commonType = randomWeight(Common);
+      if(commonType==="55"){
+        PowerSpeed = 125
+        Power = 75
+        Hardness =1 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: PowerSpeed,
+          commonType: commonType,
+        };
+      }
+      else if(commonType === "20"){
+        Speed = 130
+        Power = 80
+        Hardness = 3
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          commonType: commonType,
+        };
+      }
+      else if(commonType === "15"){
+        
+        Speed = 135
+        Power = 85
+        Hardness =5 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          commonType: commonType,
+        };
+      }
+      else if(commonType === "9"){
+        Speed = 140
+        Power = 90
+        Hardness =7 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          commonType: commonType,
+        };
+      }
+      else{
+        Speed = 145
+        Power = 95
+        Hardness =9
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          commonType: commonType,
+        };
+      }
+
+    }
+
+    else if(trainType === "Rare") {
+
+
+      let Hardness;
+      let Power;
+      let Speed;
+
+      const rareType = randomWeight(Rare);
+      if(rareType==="55"){
+        Speed = 150
+        Power = 100
+        Hardness = 11 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          rareType: rareType,
+        };
+      }
+      else if(rareType === "20"){
+        Speed = 155
+        Power = 105
+        Hardness = 13
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          rareType: rareType,
+        };
+      }
+      else if(rareType === "15"){
+        Speed = 160
+        Power = 110
+        Hardness = 15 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          rareType: rareType,
+
+        };
+      }
+      else if(rareType === "9"){
+        Speed = 165
+        Power = 110
+        Hardness =15 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          rareType: rareType,
+
+        };
+      }
+      else{
+        Speed = 170
+        Power = 120
+        Hardness =19
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          rareType: rareType,
+        };
+      }
+
+
+    }
+
+    else if(trainType === "Epic") {
+
+
+      let Hardness;
+      let Power;
+      let Speed;
+
+      const epicType = randomWeight(Epic);
+      if(epicType==="55"){
+        Speed = 175
+        Power = 125
+        Hardness = 21 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          epicType: epicType,
+        };
+      }
+      else if(epicType === "20"){
+        Speed = 180
+        Power = 130
+        Hardness = 23
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          epicType: epicType,
+        };
+      }
+      else if(epicType === "15"){
+        Speed = 185
+        Power = 135
+        Hardness = 25 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          epicType: epicType,
+        };
+      }
+      else if(epicType === "9"){
+        Speed = 190
+        Power = 140
+        Hardness = 127 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          epicType: epicType,
+        };
+      }
+      else{
+        Speed = 195
+        Power = 140
+        Hardness =27
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          epicType: epicType,
+        };
+      }
+
+
+    }
+
+
+    else if(trainType === "Legendary") {
+
+
+      let Hardness;
+      let Power;
+      let Speed;
+
+      const legendType = randomWeight(Legend);
+      if(legendType==="55"){
+        Speed = 200
+        Power = 150
+        Hardness = 31 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          legendType: legendType
+        };
+      }
+      else if(legendType === "20"){
+        Speed = 205
+        Power = 155
+        Hardness = 33
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          legendType: legendType
+        };
+      }
+      else if(legendType === "15"){
+        Speed = 210
+        Power = 160
+        Hardness = 35 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          legendType: legendType
+        };
+      }
+      else if(legendType === "9"){
+        Speed = 215
+        Power = 165
+        Hardness = 137 
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          legendType: legendType
+        };
+      }
+      else{
+        Speed = 220
+        Power = 170
+        Hardness = 39
+        nftData = {
+          nftType: type,
+          trainType: trainType,
+          Hardness: Hardness,
+          Power: Power,
+          Speed: Speed,
+          legendType: legendType
+        };
+      }
+
+
+    }
+
+    // const Hardness = Math.floor(Math.random() * 99);
+    // nftData = {
+    //   nftType: type,
+    //   trainType: trainType,
+    //   Hardness: randomInteger(20, 99),
+    //   Power: randomInteger(20, 99),
+    //   Speed: randomInteger(20, 99),
+    // };
+
+    
   } else {
     const stationType = randomWeight(Stations);
     nftData = {
@@ -97,7 +441,7 @@ for (let index = 0; index < 20; index++) {
       stationType: stationType,
     };
   }
-  console.log("type", type);
+  // console.log("type", type);
 
   // lets output this data to JSON!!!!!!!
   fs.writeFileSync(
